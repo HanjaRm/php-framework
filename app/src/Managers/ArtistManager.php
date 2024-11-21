71ab7ce4b54a956cdfde67b1867de9fa474b1db9
Edenn Touitou <edenn.touitou@letsjam.fr>
2024-11-21 13:40:25 +0100

    query builder done ?

--- app/src/Managers/ArtistManager.php
+++ app/src/Managers/ArtistManager.php
@@ -5,5 +5,13 @@ namespace App\Managers;
 use App\Lib\Managers\AbstractManager;
 
 class ArtistManager extends AbstractManager {
-
+    public function findByIdSuperiorTo(int $id) {
+        return $this->queryBuilder()
+            ->select()
+            ->from()
+            ->where('id', self::CONDITIONS['gt'])
+            ->addParam('id', $id)
+            ->executeQuery()
+            ->getAllResults();
+    }
 }
